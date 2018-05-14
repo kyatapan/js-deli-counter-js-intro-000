5b04d3a025f9c019d647b930889db2309e8e2a38
kathrin v <go.kathrin@gmail.com>
2018-05-14 00:57:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, name) {
   var positionInLine = katzDeliLine.length + 1;
   katzDeliLine.push(name);
-  return positionInLine;
+  return `Welcome, ${name}. You are number ${positionInLine} in line.`;
 }
