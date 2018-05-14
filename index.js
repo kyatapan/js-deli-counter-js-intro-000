5e0d17dc745bf34b6d9a6b1e251d0b7b2eb5d533
kathrin v <go.kathrin@gmail.com>
2018-05-14 01:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if(katzDeliLine.length===0) {
-    return `The line is currently empty.`
+    return `The line is currently empty.`;
   }
   else{
-    var announcement = 'The line is currently:'
+    var announcement = 'The line is currently:';
     for(var i=0; i<katzDeliLine.length; i++) {
-      announcement.concat(` ${i+1}. ${katzDeliLine[i]}`)
+      announcement = announcement.concat(` ${i+1}. ${katzDeliLine[i]}`);
     }
     return announcement;
   }
