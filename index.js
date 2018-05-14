67627bd8b0cddadbe90d1c4d7837280f51f4f7df
kathrin v <go.kathrin@gmail.com>
2018-05-14 00:56:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var katzDeli = [];
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
+  var positionInLine = katzDeliLine.length + 1;
   katzDeliLine.push(name);
-  return katzDeliLine.inde
+  return positionInLine;
 }
