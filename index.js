3a60d19aa297f7c891f33862fd503d37e38dc347
kathrin v <go.kathrin@gmail.com>
2018-05-14 01:10:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function nowServing(katzDeliLine) {
   }
   else {return `Currently serving ${katzDeliLine.shift()}.`}
 }
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length===0) {
+    return `The line is currently empty.`
+  }
+  else{
+    var announcement = 'The line is currently:'
+    for(var i=0; i<katzDeliLine.length; i++) {
+      
+    }
+  }
+}
