ad6f13eb0d2dcbb47d2767db78e453f2bec11815
kathrin v <go.kathrin@gmail.com>
2018-05-14 01:14:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ function currentLine(katzDeliLine) {
   else{
     var announcement = 'The line is currently:'
     for(var i=0; i<katzDeliLine.length; i++) {
-      announcement = append(` ${i+1}. ${katzDeliLine[i]}`)
+      announcement = concat(announcement, ` ${i+1}. ${katzDeliLine[i]}`)
     }
+    return announcement;
   }
 }
